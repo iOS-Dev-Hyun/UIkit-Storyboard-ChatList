48a140818f89213afd819ee9918c3cfd5c1dc41e
LeeHyunsoo <mm083434@gmail.com>
2023-10-07 03:16:52 +0900

    Initial Commit

---
+++ ChatList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatList
+//
+//  Created by HyunSoo on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
